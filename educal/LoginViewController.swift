2f725940220771393e2b6d5e7df0451147b97806
educalapps <educalapp@gmail.com>
2014-12-01 14:01:56 +0100

    view controller for login

---
+++ educal/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  educal
+//
+//  Created by Jurriaan Lindhout on 01-12-14.
+//  Copyright (c) 2014 Jurriaan Lindhout. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
