1002b73a1cc502eea569521dd3ba77856e670b0b
educalapps <educalapp@gmail.com>
2014-12-01 13:12:10 +0100

    some more changed

--- educal/ViewController.swift
+++ educal/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
         // something changed here by jurriaan !!
+        
+        
+        
+        // some more code here !!!!!!
     }
 
 
