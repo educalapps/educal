dd9a64e0b814011df89718c10e94cab17020e4d0
educalapps <educalapp@gmail.com>
2014-12-03 09:30:20 +0100

    version 3-12

--- educal/LoginViewController.swift
+++ educal/LoginViewController.swift
@@ -17,6 +17,7 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.title = "log in"
     }
     
     override func viewWillAppear(animated: Bool) {
