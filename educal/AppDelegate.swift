450b8d1cac19e20fff7e99aad5c5bbb1fe70d8da
educalapps <educalapp@gmail.com>
2014-12-09 16:33:16 +0100

    sdfsdf

--- educal/AppDelegate.swift
+++ educal/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
        
-        // Show launchscreen for 1 seconds
+        // Show launchscreen for 1 seconds 
         sleep(1);
         
         // Set Shadow
