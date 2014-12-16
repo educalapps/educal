1b5111a25b12c67ec84228d8a537f0700839b4ef
educalapps <educalapp@gmail.com>
2014-12-16 10:33:16 +0100

    courses aangepast

--- educal/AddCourseTableViewController.swift
+++ educal/AddCourseTableViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class AddCourseTableViewController: UITableViewController {
     
-    //var talbeContent = Dictionary<String,AnyObject?>()
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var codeTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    
+//    var courseInfo:Course?
+//    var groups = Array<Group>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,79 +26,88 @@ class AddCourseTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func savePressed(sender: UIBarButtonItem) {
+        
+        if !titleTextField.text.isEmpty || !codeTextField.text.isEmpty {
+            var course = PFObject(className: "Course")
+            course["userObjectId"] = PFUser.currentUser().objectId
+            course["title"] = titleTextField.text
+            course["code"] = codeTextField.text
+            course["description"] = descriptionTextField.text
+            course.saveInBackgroundWithTarget(nil, selector: nil)
+            println("saved")
+            performSegueWithIdentifier("BackToCoursesTableView", sender: sender)
+        } else {
+            Functions.Instance().showAlert("Error!", description: "Fill in the required fields.")
+        }
+
+    }
 
     // MARK: - Table view data source
-
+    /*
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 2
     }
+    */
 
+    /*
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        
+        switch section {
+            case 0:
+                return 1
+            case 1:
+                return groups.count
+            default:
+                return 0
+        }
+        
     }
+    */
 
     /*
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("CourseTextFieldCell", forIndexPath: indexPath) as UITableViewCell
 
         // Configure the cell...
-
+        switch indexPath.section {
+            case 0:
+                cell.textLabel?.text = courseInfo?.title
+                cell.detailTextLabel?.text = courseInfo?.code
+            case 1:
+                cell.textLabel?.text = groups[indexPath.row].title
+                cell.detailTextLabel?.text = groups[indexPath.row].code
+            default:
+                println("invalid section")
+        }
         return cell
     }
     */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
+    /*
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
+        
+        
     }
     */
 
